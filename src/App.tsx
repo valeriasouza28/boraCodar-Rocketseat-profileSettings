764b1916b42b7393ee477c7062e59fb65b1d35d1
valeriasouza28 <valeriasouza28@gmail.com>
2023-06-07 13:16:39 -0300

    updade

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import avatar from './assets/Avatar.png';
 import styles from './App.module.css';
 import './global.css';
@@ -14,7 +13,6 @@ import {
   Devices,
   Question,
   SignOut,
-  TShirt
 } from '@phosphor-icons/react';
 
 export function Home() {
